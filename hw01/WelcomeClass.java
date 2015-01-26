b9aa84bb2ee71d5bb023b6b9503d9e08a9b49e64
jrmannherz <jrm317@lehigh.edu>
2015-01-26 16:19:04 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+//
+//James Mannherz
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+// define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the welcome statement with specified design
+        System.out.println(" --------- ");
+        System.out.println(" |WELCOME| ");
+        System.out.println(" ---------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-J--R--M--3--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+// print a tweet length bio about yourself        
+        System.out.println("I attended a summer camp, for 9 years, called Camp Belknap in Wolfeboro,NH. There, I learned to shoot bows, guns, and how to sail, in addition to the traditional summer camp activities ");
+        
+        //test/testtest
+    }
+    
+    
+}
