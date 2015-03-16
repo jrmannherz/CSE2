613a5916c0357314df3a5b5a13c6cd5fba3ad1a6
jrmannherz <jrm317@lehigh.edu>
2015-03-16 01:23:12 +0000

    hw07

--- hw07/Waves.java
+++ hw07/Waves.java
@@ -16,48 +16,114 @@ public class Waves {
     public static void main (String[] args){
         
         Scanner myScanner = new Scanner(System.in); //initialize scanner variable 
-        int a = 0; //initialize variable a
-        int b = 0; //initialize variable b
+        int a = 0;//initialize variable a
+        int b = 0;//initialize variable b
         int x = 1;//initialize variable x
-        int T = 0; //initialize throw away variable
+        int d = 0;//initialize variable d
+        int c = 0;//initialize variable c
+        int T = 0;//initialize throw away variable
+        String f = "";//initialize string f
         String t;//initialize throw away variable t
         boolean valid = false;//initialize boolean valid
         
         
         System.out.print("Please enter a number between 1 and 30: ");//prompt user for number
-        while(!valid){
-        if(myScanner.hasNextInt()){
+         while(!valid){
+            if(myScanner.hasNextInt()){
             T = myScanner.nextInt();
-            if(T < 30  && T > 1){
-                valid = true;  
-                a = T;    
-            }// end of if statement
+                if(T < 30  && T > 1){
+                    valid = true;  
+                    a = T;    
+                }// end of if statement
+                else{
+                    b = T;
+                    System.out.println("Incorrect input, try again:");
+                    }//end else statement
+            }//close if statement
             else{
-               b = T;
-                System.out.println("Incorrect input, try again:");
-            }//end else statement
-        }//close if statement
-        else{
             t = myScanner.next();
             valid = false;
             System.out.println("Incorrect input, try again:");
             }//end else statement
-        }//close while loop
-      
-       
-        
-        int c; //initialize variable c
-        String temp = "";//initiate temp string
-        for(x=0;x<a;x++){
-            for(c=0;c<=x;c++){
-                
-            }//close for loop
-        }//close for loop
-        
-       
-      
-        
-        
+            }//close while loop
+            
+        System.out.println("For Loop");
+                   for(x=1; x<=a;x++){
+                        if(x%2==0){
+                            for (c = 1; c <= x; c++){        
+                                for(d = 1; d <= c; d ++)
+                                System.out.print (x + " ");
+                                System.out.println();
+                            }//close for loop
+                        }//close if statement
+                        else{
+                            for(c=1; c<= x;c++){
+                                for(d=1;d<=(x+1)-c; d++)
+                                System.out.print(x +" ");
+                                System.out.println();
+                            }//close for loop
+                        }//close else statement
+                   }//close the big for loop 
+                   
+                   
+        System.out.println("While Loop");
+                    x=1;
+                    while(x<=a){
+                        c=1;
+                            while(c<=x){
+                                d=1;
+                                if(x%2==0){
+                                    while(d<=c){
+                                        System.out.print(x+" ");
+                                        d++;
+                                    }//close d while loop
+                                }
+                                else{
+                                    while(d<=(x+1-c)){
+                                        System.out.print(x+" ");
+                                        d++;
+                                    }
+                                }                        
+                        
+                        
+                        c++;            
+                            System.out.println();
+                            }//close c while loop
+                    x++;    
+                    }//close x while loop
+                   
+                   
+            System.out.println("Do Loop");
+            x=1;
+            do{
+                c=1;
+                    do{
+                        d=1;
+                            if(x%2==0){
+                                do{
+                                    System.out.print(x+" ");
+                                    d++;
+                                }while(d<=c);
+                            }
+                            else{
+                                do{
+                                    System.out.print(x+" ");
+                                    d++;
+                                }while(d<=(x+1-c));
+                                
+                                
+                            }
+                            System.out.println();
+                    
+                    c++;    
+                    }while(c<=x);
+            x++;        
+            }while(x<=a);
+            
+                   
+                   
+                   
+                   
         
     }//Close Main Method
 }//Close Class
